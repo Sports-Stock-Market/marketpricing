df33287de8b2f4fe3111a5203253fb21bf5ab198
Will Cravitz <willcravitz@Wills-MacBook-Pro-3.local>
2020-04-29 01:54:42 -0500

    fixed constructors for player and team

--- objects.py
+++ objects.py
@@ -3,15 +3,21 @@ import pandas as pd
 import nba_data
 
 class Player:
-    def __init__(self, id, team, df=nba_data.players_advanced, csv='latest_RAPTOR_by_player'):
-        self.id
-        self.first_name
-        self.last_name
-        self.raptor
-        self.war
-        self.status
-        self.team
-        self.minutes
+    def __init__(self, id, team, info_df=nba_data.players_advanced, raptor_df=nba_data.players_raptor):
+        self.id = id
+        self.info = info_df.loc[info_df['PLAYER_ID'] == self.id]
+        self.first_name = self.info['PLAYER_NAME'].split()[0]
+        self.last_name = self.info['PLAYER_NAME'].split()[1]
+        self.teamid = self.info['TEAM_ID']
+        self.mpg = self.info['MIN']
+        self.adv = raptor_df.loc[raptor_df['Player_name'] == self.first_name + ' ' + self.last_name]
+        self.raptor = {
+            'offense': self.adv['Raptor_offense'],
+            'defense': self.adv['Raptor_defense'],
+            'total': self.adv['Raptor_total']
+        }
+        self.war = self.adv_stats['War_total']
+        self.status = True
     
     def injury(self, duration):
         self.status = False
@@ -33,14 +39,28 @@ class Team:
     }
 
     def __init__(self, id, players):
+        self.id = id
+        self.info = df.loc[df['TEAM_ID'] == self.id]
+        self.city = self.info['TEAM_ID'].split()[0]
+        self.name = self.info['TEAM_ID'].split()[1]
         self.players = players
         self.curr_rating = self.update_rating()
         self.proj_rating = self.update_rating(proj=True)
-        self.raptor = {}
+        self.raptor = {
+            'offense': 0,
+            'defense': 0,
+            'total': 0
+        }
+        self.update_all_raptor()
 
-    def update_rating(self, proj=False, df=nba_data.teams_advanced):
-        stats = curr_stats if time == False else stats = proj_stats
-        self.rating = 3*sum([stats[stat][0]*(row[stat]/stats[stat][1])]) 
+    def update_info(self, df=df=nba_data.teams_advanced):
+        self.info = df.loc[df['TEAM_ID'] == self.id]
+
+    def update_curr_rating(self):
+        cls.update_max_stats()
+        self.update_info()
+        weight = lambda a_dict, key: a_dict[key][0] * (self.info[key]/a_dict[key][1])
+        self.rating = 3*sum([weight(self.curr_stats, stat) for stat in self.curr_stats]) 
         return self.rating
 
     def update_raptor(self, category):
@@ -56,7 +76,7 @@ class Team:
     def update_max_stats(cls, df=nba_data.teams_advanced):
         for stat in cls.curr_stats:
             cls.curr_stats[stat][1] = df[stat].max()
-        return cls.main_stats
+        return cls.curr_stats
 
 def trade(p1, p2):
     team1 = p1.team
@@ -65,4 +85,9 @@ def trade(p1, p2):
     team2.players[team2.players.index(p2)] = p1
     p1.team = p2.team
     p2.team = team1
-    
+
+def init_players(df=nba_data.players_advanced):
+    pass
+
+def init_teams(players, df=nba_data.teams_advanced):
+    pass
