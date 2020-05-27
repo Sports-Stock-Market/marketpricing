a907c31049bd72d4bb0b9d88a2b9f39360dd7e97
Will Cravitz <willcravitz@Wills-MacBook-Pro-3.local>
2020-05-27 03:59:10 -0500

    runs season simulation

--- main.py
+++ main.py
@@ -1,62 +1,17 @@
-import numpy as np
 import pandas as pd
-import stats
+import numpy as np
+import datetime
+import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
+import nba
+import nba_data
 
-team_stat_weights = {
-    'W_PCT': 5,
-    'NET_RATING': 3,
-    'OFF_RATING': 4,
-    'DEF_RATING': 3,
-    'EFG_PCT': 2,
-    'TS_PCT': 2,
-    'PIE': 5
-}
-
-def find_max_team_stats():
-    for stat in team_stat_weights:
-        team_stat_weights[stat] = [team_stat_weights[stat], stats.teams_advanced[stat].max()]
-
-def normalize(n, stat_type):
-    if n < 0:
-        raise ValueError("rating cannot be negative")
-    if stat_type == 'player':
-        return np.log(n)**3
-    elif stat_type == 'team':
-        return n*3
-    else:
-        raise ValueError("argument stat_type must be either 'player' or 'team'")
-
-def guard_rating(row):
-    return row['USG_PCT']*(row['PIE']+((row['NET_RATING']/2)+((row['AST_PCT']+row['REB_PCT'])/12)))
-
-def forward_rating(row):
-    return row['USG_PCT']*(row['PIE']+((row['NET_RATING']/2)+((row['AST_PCT']+row['REB_PCT'])/10)))
-
-def center_rating(row):
-    return row['USG_PCT']*(row['PIE']+((row['NET_RATING']/2)+(((row['AST_PCT']/2)+(row['REB_PCT']*2.25))/12)))
-
-def initialize_team_ratings():
-    team_ratings = {}
-    find_max_team_stats()
-    for index, row in stats.teams_advanced.iterrows():
-        rating = 0
-        for stat in team_stat_weights:
-            if stat == 'NET_RATING':
-                rating += team_stat_weights[stat][0]*(row[stat]+10)/(team_stat_weights[stat][1]+10)
-            else:
-                rating += team_stat_weights[stat][0]*(row[stat]/team_stat_weights[stat][1])
-        team_ratings[row['TEAM_ID']] = normalize(rating, 'team')
-    return team_ratings
-
-def initialize_player_ratings():
-    player_ratings = {}
-    for index, row in stats.players_advanced.iterrows():
-        pos = stats.get_player_position(row['PLAYER_ID'])
-        if pos == "Guard":
-            rating = guard_rating(row)
-        elif pos == "Forward" or "Forward-Guard":
-            rating = forward_rating(row)
-        else:
-            rating = center_rating(row)
-        player_ratings[row['PLAYER_ID']] = [normalize(rating, 'player'), row['TEAM_ID']]
-    return player_ratings
+season = nba.Season(2011)
+for i in range(len(season.dates)):
+    for j in range(i, len(season.dates)):
+        for game in season.games[season.dates[j]]:
+            game.predict()
+    for index, row in season.stats[season.dates[i]].iterrows():
+        team = nba.find_team(season.teams, row['TEAM_NAME'])
+        team.update_info()
+        team.update_rating()
